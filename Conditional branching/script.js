174a72d60bd83004cba103e0e4c67ba193ca114f
Slava <slava.teplov.2014@mail.ru>
2022-06-20 00:13:50 +0300

    conditional branching

---
+++ Conditional branching/script.js
@@ -0,0 +1,9 @@
+//Задача 1 (if (строка с нулём))
+//Выведется, так как "0" строка и она равна true
+
+
+//Задача 2 (Название JavaScript)
+
+let result = prompt("Какое 'официальное' название JavaScript?");
+
+(result == "ECMAScript")? alert('Верно!') : alert('Не знаете? ECMAScript!');
