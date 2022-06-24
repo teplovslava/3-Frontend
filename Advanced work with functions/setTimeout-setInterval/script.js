005a03442e983c31a60651b0c023b3b28a1ca8f5
Slava <slava.teplov.2014@mail.ru>
2022-06-24 16:09:21 +0300

    add

--- Advanced work with functions/setTimeout-setInterval/script.js
+++ Advanced work with functions/setTimeout-setInterval/script.js
@@ -1,2 +1,28 @@
 //Задание 1
-function printNumbers(from, to)
+function printNumbers(from, to) {
+    let current = from;
+  
+    let timerId = setInterval(function() {
+      alert(current);
+      if (current == to) {
+        clearInterval(timerId);
+      }
+      current++;
+    }, 1000);
+  }
+//рекурсивный способ
+function printNumbers(from, to) {
+    let current = from;
+  
+    setTimeout(function go() {
+      alert(current);
+      if (current < to) {
+        setTimeout(go, 1000);
+      }
+      current++;
+    }, 1000);
+  }
+
+
+//Задание 2
+//после завершения кода for
